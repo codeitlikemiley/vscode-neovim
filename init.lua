28980c5b17f4f75ac79c2fb6e5833106333f75d2
Uriah <codeitlikemiley@gmail.com>
2024-01-01 18:03:52 +0800

    global functions

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 if vim.g.vscode then
-    cargo_run = require("utils/cargo_run")
-    cargo_nextest = require("utils/cargo_nextest")
+  cargo_run = require("utils/cargo_run")
+  cargo_nextest = require("utils/cargo_nextest")
 end
+
