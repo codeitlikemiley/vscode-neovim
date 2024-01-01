95b3082d0243c665b355b5db03c32a4966c797e3
Uriah <codeitlikemiley@gmail.com>
2024-01-01 18:06:03 +0800

    disable plugins not needed by vcode

---
+++ lua/plugins/disable.lua
@@ -0,0 +1,70 @@
+if vim.g.vscode == 1 then
+  return {
+    {
+      "echasnovski/mini.surround",
+      enabled = false,
+    },
+    {
+      "echasnovski/mini-comment",
+      enabled = false,
+    },
+    {
+      "echasnovski/mini-pairs",
+      enabled = false,
+    },
+    {
+      "echasnovski/mini.surround",
+      enabled = false,
+    },
+    {
+      "tpope/vim-repeat",
+      enabled = false,
+    },
+    {
+      "echasnovski/mini.ai",
+      enabled = false,
+    },
+    {
+      "echasnovski/mini.ai",
+      enabled = false,
+    },
+    {
+      "echasnovski/mini.nvim",
+      enabled = false,
+    },
+    {
+      "echasnovski/mini.surround",
+      enabled = false,
+    },
+    {
+      "akinsho/bufferline.nvim",
+      enabled = false,
+    },
+    {
+      "nvimdev/dashboard-nvim",
+      enabled = false,
+    },
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      enabled = false,
+    },
+    {
+      "folke/noice.nvim",
+      enabled = false,
+    },
+    {
+      "MunifTanjim/nui.nvim",
+      enabled = false,
+    },
+    {
+      "rcarriga/nvim-notify",
+      enabled = false,
+    },
+    {
+      "folke/tokyonight.nvim",
+      enabled = false,
+    },
+  }
+else
+  return {}
+end
