4a5b3271eddb192404e20af580006554a41753f1
Uriah <codeitlikemiley@gmail.com>
2024-04-05 03:20:25 +0800

    remove redo

--- lua/config/keymaps/unredo.lua
+++ lua/config/keymaps/unredo.lua
@@ -3,5 +3,4 @@ if vim.fn.has("macunix") == 1 and vim.fn.exists("neovide") == 1 then
   vim.keymap.set("i", "<D-z>", "<Esc>: u<CR>", { silent = true, desc = "Undo" })
 else
   vim.keymap.set("n", "<C-z>", "u", { silent = true, desc = "Undo" })
-  vim.keymap.set("n", "<C-y>", "<C-r>", { silent = true, desc = "Redo" })
 end
