563d27a75d187a04fe051e235581f5a8c2f2b587
Uriah <codeitlikemiley@gmail.com>
2024-04-05 03:18:39 +0800

    remove constraint for neovide

--- lua/config/keymaps/clipboard.lua
+++ lua/config/keymaps/clipboard.lua
@@ -1,6 +1,5 @@
-if vim.fn.has("macunix") == 1 and vim.fn.exists("neovide") == 1 then
+if vim.fn.has("macunix") == 1  then
   vim.keymap.set("v", "<D-c>", '"+y') -- Copy on visual mode
-
   vim.keymap.set("n", "<D-v>", '"+P') -- Paste normal mode
   vim.keymap.set("v", "<D-v>", '"+P') -- Paste visual mode
   vim.keymap.set("i", "<D-v>", '<ESC>l"+Pli') -- Paste insert mode
